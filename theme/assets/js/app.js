44b942cbe548c72d752ec1748f30d702139977a5
ruanyf <yifeng.ruan@gmail.com>
2016-12-07 16:29:12 +0800

    style: adjust page layout

--- theme/assets/js/app.js
+++ theme/assets/js/app.js
@@ -1,3 +1,20 @@
+/* set content's min-height */
+var content = document.querySelector('.content');
+var navbarHeight =
+  window.getComputedStyle(document.querySelector('.navbar'))
+  .getPropertyValue('height');
+var footHeight =
+  window.getComputedStyle(document.querySelector('.foot'))
+  .getPropertyValue('height');
+var showNavigationHeight =
+  window.getComputedStyle(document.querySelector('.show-navigation'))
+  .getPropertyValue('height');
+var minHeight = document.documentElement.clientHeight
+  - parseInt(navbarHeight)
+  - parseInt(footHeight)
+  - (parseInt(showNavigationHeight) ? parseInt(showNavigationHeight) : 0);
+content.style.minHeight = minHeight + 'px';
+
 /* insert article's toc */
 var article = document.querySelector('.article');
 var firstH2Title = article.querySelector('h2');
@@ -9,3 +26,20 @@ if (firstH2Title && LOPPO.article_toc && h2TitleNumber.length >= 2) {
   toc_div.innerHTML = '<h3>Contents</h3>' + LOPPO.article_toc;
   article.insertBefore(toc_div, firstH2Title);
 }
+
+/* show_navigation */
+var show_navigation_button = document.querySelector('.show-navigation .button');
+if (show_navigation_button) {
+  show_navigation_button.onclick = function (e) {
+    if (show_navigation_button.textContent === 'Show Navigation') {
+      show_navigation_button.textContent = 'Hide Navigation';
+    } else {
+      show_navigation_button.textContent = 'Show Navigation';
+    }
+    var book_toc = document.querySelector('.book-toc');
+    book_toc.style.borderBottom = '1px solid #dbdbdb';
+    var classList = book_toc.classList;
+    classList.toggle('is-hidden-touch');
+  }
+}
+
